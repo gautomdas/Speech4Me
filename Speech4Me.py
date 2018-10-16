91adad049993858e2b65b473eb4553a70d4261fe
Bryan Li <bryanli0@icloud.com>
2018-10-16 18:57:30 -0400

    Fixed infoboxes.

--- Speech4Me.py
+++ Speech4Me.py
@@ -37,10 +37,21 @@ def stats():
     con_gen_text = [data[1][2], data[1][3], data[1][4]]
 
     all_labels = ['fre_s', 'fre_g', 'fkg_s', 'fkg_g', 'gfi_s', 'gfi_g', 'si_s', 'si_g', ' ari_s', 'ari_g', 'cli_s', 'cli_g', 'lwi_s', 'lwi_g', 'dcr_s', 'dcr_g']
-    all_datas = [data[2][0], data[3][0], data[4][0], data[5][0], data[6][0], data[7][0], data[8][0], data[9][0]]
+    all_datas = [data[2][2], data[3][2], data[4][2], data[5][2], data[6][2], data[7][2], data[8][2], data[9][2], data[10][2]]
     print("++++++: "+str(all_datas))
     print(gen_text_info)
-    return render_template('stats.html', comp=data[0][0], neg=data[0][1], pos=data[0][2], neut=data[0][3], sent=data[1][0], sent_len=data[1][1], syll=data[1][2], word=data[1][3], char=data[1][4], char_nospc=data[1][5], avg_c=data[1][6], syll_per_word=data[1][7], fre_s=data[2][0], fre_g=data[2][1], fkg_s=data[3][0], fkg_g=data[3][1], gfi_s=data[4][0], gfi_g=data[4][1], si_s=data[5][0], si_g=data[5][1], ari_s=data[6][0], ari_g=data[6][1], cli_s=data[7][0], cli_g=data[7][1], lwi_s=data[8][0], lwi_g=data[8][1], dcr_s=data[9][0], dcr_g=data[9][1], dataA = text_sentiment, gen_text=gen_text_info, all_l=all_labels, all_d=all_datas, over=data[10][1], cont_gen=con_gen_text, avg_sl=data[1][1], avg_syll=data[1][7])
+    return render_template('stats.html', comp=data[0][0], neg=data[0][1],pos=data[0][2], neut=data[0][3],
+                           sent=data[1][0], sent_len=data[1][1], syll=data[1][2], word=data[1][3], char=data[1][4],
+                           char_nospc=data[1][5], avg_c=data[1][6], syll_per_word=data[1][7], fre_s=data[2][0],
+                           fre_g=data[2][1], fkg_s=data[3][0], fkg_g=data[3][1], gfi_s=data[4][0],
+                           gfi_g=data[4][1], si_s=data[5][0], si_g=data[5][1], ari_s=data[6][0], ari_g=data[6][1],
+                           cli_s=data[7][0], cli_g=data[7][1], lwi_s=data[8][0], lwi_g=data[8][1],
+                           dcr_s=data[9][0], dcr_g=data[9][1], dataA = text_sentiment, gen_text=gen_text_info,
+                           all_l=all_labels, all_d=all_datas, over=data[10][1], overG=data[10][0],
+                           cont_gen=con_gen_text, avg_sl=data[1][1], avg_syll=data[1][7],
+                           fre_gph=data[2][2], fkg_gph=data[3][2], gfi_gph=data[4][2], si_gph=data[5][2],
+                           ari_gph=data[6][2], cli_gph=data[7][2], lwi_gph=data[8][2], dcr_gph=data[9][2],
+                           overGph=data[10][2])
 
 
 
--- TextAnalysis.py
+++ TextAnalysis.py
@@ -42,31 +42,32 @@ class TextAnalysis:
     # [INDEX 2] Syllable count          [INDEX 3] Overall word count
     # [INDEX 4] Character count
     ##### INDEX 2 IN DATA: Flesch Reading Ease #####
-    # [INDEX 0] Pure score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 100
     ##### INDEX 3 IN DATA: Flesch-Kincaid Grade #####
-    # [INDEX 0] Pure score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 18
     ##### INDEX 4 IN DATA: Gunning FOG Index #####
-    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 18
     ##### INDEX 5 IN DATA: SMOG Index #####
-    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 18
     ##### INDEX 6 IN DATA: Automated Readability Index #####
-    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 14
     ##### INDEX 7 IN DATA: Coleman-Liau Index #####
-    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 18
     ##### INDEX 8 IN DATA: Linsear Write Index #####
-    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 18
     ##### INDEX 9 IN DATA: Dale-Chall Readability Score #####
-    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
     # SCORE SCALE: 0 - 10
     ##### INDEX 10 IN DATA: Overall Score #####
-    # Approximate grade (NOT A LIST)
+    # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
+    # SCORE SCALE: 0 - 20
 
 
     def updateData(self):
@@ -90,59 +91,68 @@ class TextAnalysis:
                           textstat.avg_syllables_per_word(self.text)])
 
         ##### INDEX 2 IN DATA: Flesch Reading Ease #####
-        # [INDEX 0] Pure score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 100
-        self.freStat = min(max(textstat.flesch_reading_ease(self.text), 0), 100)
-        self.data.append([self.freStat, self.freGrade(self.freStat)])
+        self.freRaw = textstat.flesch_reading_ease(self.text)
+        self.freStat = abs(min(max(self.freRaw, 0), 100) - 100)
+        self.data.append([self.freRaw, self.freGrade(self.freStat), self.freStat])
 
         ##### INDEX 3 IN DATA: Flesch-Kincaid Grade #####
-        # [INDEX 0] Pure score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 18
-        self.fkgStat = self.adjustScore(textstat.flesch_kincaid_grade(self.text))
-        self.data.append([self.fkgStat, self.grade(self.fkgStat)])
+        self.fkgRaw = textstat.flesch_kincaid_grade(self.text)
+        self.fkgStat = self.adjustScore(self.fkgRaw)
+        self.data.append([self.fkgRaw, self.grade(self.fkgStat), self.fkgStat/0.18])
 
         ##### INDEX 4 IN DATA: Gunning FOG Index #####
-        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 18
-        self.fogStat = self.adjustScore(textstat.gunning_fog(self.text))
-        self.data.append([self.fogStat, self.grade(self.fogStat)])
+        self.fogRaw = textstat.gunning_fog(self.text)
+        self.fogStat = self.adjustScore(self.fogRaw)
+        self.data.append([self.fogRaw, self.grade(self.fogStat), self.fogStat/0.18])
 
         ##### INDEX 5 IN DATA: SMOG Index #####
-        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 18
-        self.smogStat = self.adjustScore(textstat.smog_index(self.text))
-        self.data.append([self.smogStat, self.grade(self.smogStat)])
+        self.smogRaw = textstat.smog_index(self.text)
+        self.smogStat = self.adjustScore(self.smogRaw)
+        self.data.append([self.smogRaw, self.grade(self.smogStat), self.smogStat/0.18])
 
         ##### INDEX 6 IN DATA: Automated Readability Index #####
-        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 14
-        self.ariStat = min(max(textstat.automated_readability_index(self.text), 0), 14)
-        self.data.append([self.ariStat, self.ariGrade(ceil(self.ariStat))]) #13
+        self.ariRaw = textstat.automated_readability_index(self.text)
+        self.ariStat = min(max(self.ariRaw, 0), 14)
+        self.data.append([self.ariRaw, self.ariGrade(ceil(self.ariStat)), self.ariStat/0.14]) #13
 
         ##### INDEX 7 IN DATA: Coleman-Liau Index #####
-        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 18
-        self.cliStat = self.adjustScore(textstat.coleman_liau_index(self.text))
-        self.data.append([self.cliStat, self.grade(self.cliStat)])
+        self.cliRaw = textstat.coleman_liau_index(self.text)
+        self.cliStat = self.adjustScore(self.cliRaw)
+        self.data.append([self.cliRaw, self.grade(self.cliStat), self.cliStat/0.18])
 
         ##### INDEX 8 IN DATA: Linsear Write Index #####
-        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 18
-        self.lwiStat = self.adjustScore(textstat.linsear_write_formula(self.text))
-        self.data.append([self.lwiStat, self.grade(self.lwiStat)])
+        self.lwiRaw = textstat.linsear_write_formula(self.text)
+        self.lwiStat = self.adjustScore(self.lwiRaw)
+        self.data.append([self.lwiRaw, self.grade(self.lwiStat), self.lwiStat/0.18])
 
         ##### INDEX 9 IN DATA: Dale-Chall Readability Score #####
-        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 10
-        self.dcrStat = min(max(textstat.dale_chall_readability_score(self.text), 0), 10)
-        self.data.append([self.dcrStat, self.grade(self.dcrStat)])
+        self.dcrRaw = textstat.dale_chall_readability_score(self.text)
+        self.dcrStat = min(max(self.dcrRaw, 0), 10)
+        self.data.append([self.dcrRaw, self.grade(self.dcrStat), self.dcrStat/0.1])
 
         ##### INDEX 10 IN DATA: Overall Score #####
-        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade
+        # [INDEX 0] Pure Score              [INDEX 1] Approximate grade     [INDEX 2] Normalized (ratio) score
         # SCORE SCALE: 0 - 20
-        self.txtStd = min(max(textstat.text_standard(self.text, True), 0), 20)
+        self.txtRaw = textstat.text_standard(self.text, True)
+        self.txtStd = min(max(self.txtRaw, 0), 20)
         self.txtInfo = textstat.text_standard(self.text)
-        self.data.append([self.txtStd, self.txtGrade(self.txtStd, self.txtInfo)])
+        self.data.append([self.txtRaw, self.txtGrade(self.txtStd, self.txtInfo), self.txtStd/0.2])
 
         return self.data
 
